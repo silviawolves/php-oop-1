910e9c407b4f3a750f45789fcdcb90992f1bf6da
silviawolves <silviacanepa.w@gmail.com>
2022-07-13 15:15:14 +0200

    completo

--- index.php
+++ index.php
@@ -7,7 +7,18 @@ $film1->castPrincipale = ['Amy Adams', 'Jake Gyllenhaal'];
 $film1->annoUscita = 2016;
 $film1->durata = '116min';
 
-echo '<h3>Il film ' . $film1->titolo . ', con protagonisti ' . $film1->castPrincipale[0] . ' e ' . $film1->castPrincipale[1] . ' è uscito ' . $film1->getYearInterval() . ' anni fa.</h3>';
+var_dump($film1);
+
+echo '<h3>Il film ' . $film1->titolo . ', con protagonisti ' . $film1->castPrincipale[0] . ' e ' . $film1->castPrincipale[1] . ', è uscito ' . $film1->getYearInterval() . ' anni fa.</h3>';
+
+$film2 = new Movie ('Drive', 'Drive');
+$film2->castPrincipale = ['Ryan Gosling', 'Carey Mulligan', 'Oscar Isaac'];
+$film2->annoUscita = 2011;
+$film2->durata = '100min';
+
+var_dump($film2);
+
+echo '<h3>Il film ' . $film2->titolo . ', con protagonisti ' . $film2->castPrincipale[0] . ', ' . $film2->castPrincipale[1] . ' e ' . $film2->castPrincipale[2] . ', è uscito ' . $film2->getYearInterval() . ' anni fa.</h3>';
 
 ?>
 
@@ -19,4 +30,4 @@ echo '<h3>Il film ' . $film1->titolo . ', con protagonisti ' . $film1->castPrinc
     => all’interno della classe sono dichiarate delle variabili d’istanza X
     => all’interno della classe è definito un costruttore X
     => all’interno della classe è definito almeno un metodo X
-- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà --> X
